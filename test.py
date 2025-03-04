84781f2a60774990fd296dcd0d34b611b0cb44f5
almondsaction <pradhan.aashish@gmail.com>
2025-03-04 17:28:59 +0000

    Update test.py

--- test.py
+++ test.py
@@ -14,25 +14,31 @@ df = pd.DataFrame(data)
 # Plot
 fig, ax1 = plt.subplots(figsize=(10, 5))
 
-# First Y-axis
+# First Y-axis (Primary)
 sns.lineplot(x=df['time'], y=df['size'], ax=ax1, color='b', label='Size')
 ax1.set_ylabel('Size', color='b')
 ax1.tick_params(axis='y', labelcolor='b')
 
 # Second Y-axis
 ax2 = ax1.twinx()
-sns.lineplot(x=df['time'], y=df['value1'], ax=ax2, color='r', label='Value1')
+line1, = ax2.plot(df['time'], df['value1'], color='r', label='Value1')  # Save handle for legend
 ax2.set_ylabel('Value1', color='r')
 ax2.tick_params(axis='y', labelcolor='r')
 
 # Third Y-axis
 ax3 = ax1.twinx()
-ax3.spines['right'].set_position(('outward', 60))  # Offset the third axis
-sns.lineplot(x=df['time'], y=df['value2'], ax=ax3, color='g', label='Value2')
+ax3.spines['right'].set_position(('outward', 60))  # Offset third axis
+line2, = ax3.plot(df['time'], df['value2'], color='g', label='Value2')  # Save handle for legend
 ax3.set_ylabel('Value2', color='g')
 ax3.tick_params(axis='y', labelcolor='g')
+
+# Combining Legends
+lines = [line1, line2]
+labels = [line.get_label() for line in lines]
+ax2.legend(lines, labels, loc='upper left', bbox_to_anchor=(1.1, 1))
 
 # Formatting
 ax1.set_xlabel('Time')
 ax1.set_xticklabels(df['time'].dt.strftime('%H:%M'), rotation=45)
-fig.tight_layou
+fig.tight_layout()
+plt.show()
